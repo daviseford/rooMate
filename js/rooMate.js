e0c4e6186f80c176e2db6f7d83d4ea162d05cc5d
daviseford <davis.e.ford.alt@gmail.com>
2015-02-05 11:23:13 -0500

    added js

---
+++ js/rooMate.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+//	$("#clickme").button();
+//	$("#clickme").click(function(){
+//	//trying some ajax?
+//	$.ajax({
+//  		type: "POST",
+//  		url: "https://mandrillapp.com/api/1.0/messages/send.json",
+//  		data: {
+//    		'key': 'MXTAqFwwNNGZdGtKOzG_Jw',
+//    		'message': {
+//      			'from_email': 'digitalmedia@wjmassociates.com',
+//      			'to': [
+//          		{
+//            		'email': 'daviseford@gmail.com',
+//            		'name': 'Davis Ford',
+//            		'type': 'to'
+//          		},
+//          		{
+//            		'email': 'alkillin@gmail.com',
+//            		'name': 'Amanda Killingsworth',
+//            		'type': 'to'
+//          		}
+//        	],
+//      'autotext': 'true',
+//      'subject': 'YOUR SUBJECT HERE!',
+//      'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
+//    		}}
+// 		}).done(function(response) {
+//   			console.log(response); // if you're into that sorta thing
+// 		});
+//	});
